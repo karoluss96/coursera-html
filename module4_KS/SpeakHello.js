13446f03eec16886a843ab81071ff77e0b96a647
karoluss96 <33833354+karoluss96@users.noreply.github.com>
2020-07-17 11:36:48 +0200

    Update SpeakHello.js

--- module4_KS/SpeakHello.js
+++ module4_KS/SpeakHello.js
@@ -1,12 +1,12 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 (function (window) {
-var names = ["Yaakov", "Paul", "Frank", "Larry", "Paula", "Laura"];
+//var names = ["Yaakov", "Paul", "Frank", "Larry", "Paula", "Laura"];
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
-var helloSpeaker = {};
+var helloSpeaker = {names: "Yaakov", "Paul", "Frank", "Larry", "Paula", "Laura"};
  helloSpeaker.speak= names; 
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
