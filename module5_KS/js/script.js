9d022cd0a335d1fd1c7112afd35a42f7dc01586f
karoluss96 <33833354+karoluss96@users.noreply.github.com>
2020-07-21 11:36:05 +0200

    Update script.js

--- module5_KS/js/script.js
+++ module5_KS/js/script.js
@@ -125,7 +125,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      insertHTML("#main-content",  homeHtmlToInsertIntoMainPage);
+      insertHtml("#main-content",  homeHtmlToInsertIntoMainPage);
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
